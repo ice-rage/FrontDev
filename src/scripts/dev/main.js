fd0a87bf4af1012a0698a6245ad5c84dc8a29c5d
ice-rage <otulissa2012@yandex.ru>
2024-02-05 20:35:42 +0700

    Adding JS code to handle the display of the event popup

--- src/scripts/dev/main.js
+++ src/scripts/dev/main.js
@@ -5,7 +5,61 @@
 
   const navToggle = document.querySelector("#js-navToggle");
 
-  navToggle.addEventListener("click", function() {
+  navToggle.addEventListener("click", function () {
     root.classList.toggle("show-nav");
   });
+  
+
+  const eventPP = document.querySelector("#js-eventPP");
+  const eventOpenBtn = document.querySelector("#js-openEventBtn");
+
+  if (eventPP && eventOpenBtn) {
+    const closeEventPP = function (event) {
+      function close() {
+        document.removeEventListener("keyup", closeEventPP);
+        eventPP.removeEventListener("click", closeEventPP);
+        
+        root.classList.remove("show-event-popup");
+      }
+
+      switch (event.type) {
+        case "keyup":
+          if (event.key === "Escape" || event.keyCode === 27) close();
+          break;
+        case "click":
+          if (
+            event.target === this ||
+            event.target.classList.contains("js-ppCloseBtn")
+          )
+            close();
+          break;
+      }
+    };
+
+    eventOpenBtn.addEventListener("click", function () {
+      root.classList.add("show-event-popup");
+
+      document.addEventListener("keyup", closeEventPP);
+      eventPP.addEventListener("click", closeEventPP);
+    });
+
+    // eventOpenBtn.addEventListener("click", function() {
+    //    root.classList.add("show-event-popup");
+    // });
+
+    // eventPP.addEventListener("click", function(event) {
+    //   if (event.target === this ||
+    //     event.target.classList.contains("js-ppCloseBtn")
+    //   ) {
+    //     root.classList.remove("show-event-popup");
+    //   }
+    // });
+
+    // document.addEventListener("keyup", function(event) {
+    //   if (event.key === "Escape" || event.keyCode === 27) {
+    //     console.log(event);
+    //     root.classList.remove("show-event-popup");
+    //   }
+    // });
+  }
 })();
